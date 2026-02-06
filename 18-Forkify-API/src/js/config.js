880651c985ca2962f78cb38049ae09046d3661f7
Elddar00 <eldaridrizovic00@gmail.com>
2026-02-06 17:12:05 +0100

    JS practice

--- 18-Forkify-API/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
-export const KEY = 'f61633f3-f308-4664-ab4a-ef23b6ffa86f';
-export const MODAL_CLOSE_SEC = 2.5;
