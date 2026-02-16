c455716e79d7f27d472b6fe16e2c32206cef0277
Elddar00 <eldaridrizovic00@gmail.com>
2026-02-16 18:24:54 +0100

    More about Array and Loops

--- js-example/scripts/11-todo-list.js
+++ js-example/scripts/11-todo-list.js
@@ -1,4 +1,13 @@
-const todoList = ["make dinner", "wash dishes"];
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "2022-12-22",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "2022-12-22",
+  },
+];
 
 renderTodoList();
 
@@ -6,12 +15,22 @@ function renderTodoList() {
   let todoListHTML = "";
 
   for (let i = 0; i < todoList.length; i++) {
-    const todo = todoList[i];
-    const html = `<p>${todo}</p>`;
+    const todoObject = todoList[i];
+
+    // const name = todoObject.name;
+    // const dueDate = todoObject.dueDate;
+
+    const { name, dueDate } = todoObject;
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+     todoList.splice(${i}, 1);
+     renderTodoList();
+    " class="delete-todo-button">Delete</button>
+    `;
     todoListHTML += html;
   }
-
-  // console.log(todoListHTML);
 
   document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 }
@@ -21,13 +40,23 @@ function addTodo() {
 
   const name = inputElement.value;
 
-  todoList.push(name);
-  console.log(todoList);
+  const dateInputElement = document.querySelector(".js-due-date-input");
+
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name,
+    // dueDate: dueDate,
+    name,
+    dueDate,
+  });
+
   inputElement.value = "";
 
   renderTodoList();
 }
 
+/*
 // 11a
 const nekiNiz = [10, 20, 30];
 
@@ -174,3 +203,24 @@ function minMax(nums) {
 console.log(minMax([1, -3, 5]));
 console.log(minMax([]));
 console.log(minMax([3]));
+
+//11n
+
+function countWords(words) {
+  const result = {};
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+
+    if (!result[word]) {
+      result[word] = 1;
+    } else {
+      result[word]++;
+    }
+  }
+
+  return result;
+}
+
+console.log(countWords(["apple", "grape", "apple"]));
+ */
